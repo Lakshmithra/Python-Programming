a4f3be0504b9eb7a729ba92ad5df396191255e1f
Lakshmithra <lakshmithraramesh2007@gmail.com>
2025-10-01 12:53:43 +0530

    Update and rename Basics/basics4.py to Basics/Loops/sum_10.py

---
+++ Basics/Loops/sum_10.py
@@ -0,0 +1,9 @@
+# Program to calculate the sum of first 10 natural numbers using while loop
+
+sum = 0
+i = 1
+while i <= 10 :
+    sum = sum + i
+    i = i + 1
+print("The sum of first 10 natural numbers is ",sum)
+
