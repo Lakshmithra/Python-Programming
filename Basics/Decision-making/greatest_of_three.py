b0bf96f72c737f25cd2894fcba0eb200c1649363
Lakshmithra <lakshmithraramesh2007@gmail.com>
2025-10-01 12:50:52 +0530

    Create greatest_of_three.py

---
+++ Basics/Decision-making/greatest_of_three.py
@@ -0,0 +1,13 @@
+a = int (input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: ")) 
+if (a > b):
+    if (a > c):
+        print (f"{a} is the greatest number ")
+if (b > a):
+    if (b > c):
+        print (f"{b} is the greatest number ")
+if (c > a):
+    if (c > b):
+        print (f"{c} is the greatest number ")
+
