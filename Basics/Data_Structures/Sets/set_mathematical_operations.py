5a3e4d3ef621c474848edba6bfdb3c2b5eb86f11
Lakshmithra <lakshmithraramesh2007@gmail.com>
2025-11-01 14:49:42 +0530

    Create set_mathematical_operations.py

---
+++ Basics/Data_Structures/Sets/set_mathematical_operations.py
@@ -0,0 +1,21 @@
+# Program to demonstrate set mathematical operations
+
+a = {1,2,3,4,5}
+b = {2,4,6,8,10}
+
+
+# Union: combines all unique elements from both sets
+
+print("Union : ", a | b)
+
+# Intersection: elements common to both sets
+
+print("Intersection : " ,a & b)
+
+# Difference: elements in 'a' but not in 'b'
+
+print("Difference : ", a - b)
+
+# Symmetric Difference: elements in either set, but not in both
+
+print("Symmetric Difference : " , a ^ b)
