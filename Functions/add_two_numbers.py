78d608565a8d1768ab654ff082d999d62b2c160c
Lakshmithra <lakshmithraramesh2007@gmail.com>
2025-11-04 21:17:00 +0530

    Create add_two_numbers.py

---
+++ Functions/add_two_numbers.py
@@ -0,0 +1,9 @@
+# Program to find the sum of two numbers
+
+def add(n1 , n2):
+    return n1 + n2
+  
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+c = add(a , b)
+print(f"Sum of {a} and {b} : {c}")
